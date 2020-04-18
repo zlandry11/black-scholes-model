7552c612bba68646f0e65edb344e3fa951e57967
Zach Landry <zlandry@haverford.edu>
2020-04-17 23:46:31 -0500

    commit

--- black-scholes.py
+++ black-scholes.py
@@ -1,22 +1,12 @@
-"""
-    The Black-Scholes model was first introduced by economists Fischer Black and Myron Scholes
-in 1973. This partial-differential equation (PDE) gives a theoretical estimate for the fair
-price of a European option. This model has become widely used by investors today and is still
-one of the most popular formulas for pricing options. Several assumptions must be made in this
-model to determine the price of European options:
-    1. Options can only be exercised at expiration
-    2. Risk-free interest rate and volatility are constant
-    3. Stock price follows Geometric Brownian Motion
-    4. No dividends are paid for the duration of the option
-    5. No transaction or commission 
-    6. No arbitrage opportunities exist
-"""
 import math
 from scipy.stats import norm
 
 def fair_price(S, X, T, r, sigma, option='call'):
     """Finds fair price of a European call or put option.
 
+    User can select from different types of options and also 
+    whether or not the underlying stock pays dividends. 
+    
     Args:
         S: Initial price of the underlying asset
         X: Strike price of the option
